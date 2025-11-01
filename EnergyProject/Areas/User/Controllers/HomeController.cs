44d00617cf3f0b1b46a6e9cbd1e44f68cd041331
Krezub Artem <artemkrezub790@gmail.com>
2025-11-01 13:28:04 +0100

    Created Profile page

--- EnergyProject/Areas/User/Controllers/HomeController.cs
+++ EnergyProject/Areas/User/Controllers/HomeController.cs
@@ -1,18 +1,32 @@
 using System.Diagnostics;
+using EnergyProject.Data;
 using EnergyProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 
 namespace EnergyProject.Areas.User.Controllers;
 [Area("User")]
-public class  HomeController : Controller
+public class HomeController : Controller
 {
-    public IActionResult Accounts() => View();
-    public IActionResult Meters()     => View();
-    public IActionResult CreateBill() => View();
-    public IActionResult Home() => View();
-    public IActionResult Profile() => View();
-    public IActionResult Readings() => View();
+    //public IActionResult Accounts() => View();
+    //public IActionResult Meters()     => View();
+    //public IActionResult CreateBill() => View();
+
+    ApplicationDbContext db;
+    public HomeController(ApplicationDbContext db_) {
+       db = db_;
+    }
+    public IActionResult Home() {
+        return View(); 
+    }
+    public IActionResult Profile() {
+        EnergyProject.Models.User user1;
+        user1 = db.Users.First();
+        // To do
+        return View(user1); 
+    }
+    /*public IActionResult Readings() => View();
     public IActionResult Support() => View();
     public IActionResult Consumption() => View();
-    public IActionResult Cards() => View();
+    public IActionResult Cards() => View();*/
 }
