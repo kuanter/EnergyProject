ec6fd1431d391384f7d4edd6a399950dd829a373
Krezub Artem <artemkrezub790@gmail.com>
2025-12-05 21:27:44 +0100

    Added create tariff ror admin

--- EnergyProject/Areas/Admin/Controllers/TariffController.cs
+++ EnergyProject/Areas/Admin/Controllers/TariffController.cs
@@ -2,6 +2,7 @@
 using EnergyProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EnergyProject.Areas.Admin.Controllers
 {
@@ -28,6 +29,18 @@ namespace EnergyProject.Areas.Admin.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Show");
             }
+            return RedirectToAction("Show");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult CreatePost(Tariff t) 
+        {
+            t.Id = Guid.NewGuid().ToString();
+            db.Tariffs.Add(t);
+            db.SaveChanges();
             return RedirectToAction("Show");
         }
     }
