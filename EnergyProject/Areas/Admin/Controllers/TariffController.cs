2a18d87365656f382d3470649fc9adec527d4c05
Krezub Artem <artemkrezub790@gmail.com>
2025-12-05 21:04:15 +0100

    Added show tariff for Admin

---
+++ EnergyProject/Areas/Admin/Controllers/TariffController.cs
@@ -0,0 +1,22 @@
+﻿using EnergyProject.Data;
+using EnergyProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnergyProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TariffController : Controller
+    {
+        public ApplicationDbContext db;
+        public TariffController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+        public IActionResult Show()
+        {
+            var Tarriffs = db.Tariffs.ToList();
+            return View(Tarriffs);
+        }
+    }
+}
