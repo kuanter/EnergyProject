07b680c494d8ceb8193294057e01d674a9d84927
Krezub Artem <artemkrezub790@gmail.com>
2025-12-12 15:58:39 +0100

    Added CardDataController and Show

---
+++ EnergyProject/Areas/Client/Controllers/CardDataController.cs
@@ -0,0 +1,21 @@
+﻿using EnergyProject.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnergyProject.Areas.Client.Controllers
+{
+    [Area("Client")]
+    public class CardDataController : Controller
+    {
+
+        ApplicationDbContext db;
+        public CardDataController(ApplicationDbContext db_)
+        {
+            db = db_;
+        }
+        public IActionResult Show()
+        {
+            var Cards = db.CardDatas;
+            return View(Cards);
+        }
+    }
+}
---
+++ EnergyProject/Areas/Client/Views/CardData/Show.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EnergyProject.Areas.Client.Views.Card
+{
+    public class ShowModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
